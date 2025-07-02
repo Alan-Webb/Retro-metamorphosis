689055fe9d73c971694c8ebac309d5217022e4bc
Alan Webb <webbalan91939@gmail.com>
2025-07-02 14:00:49 +0100

    Update Testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Services from "./components/Services";
 import Features from "./components/Features";
 import Testimonials from "./components/Testimonials";
 import Pricing from "./components/Pricing";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -18,7 +17,6 @@ const App = () => {
 			<Features />
 			<Pricing />
 			<Testimonials />
-			<Contact />
 			<Footer />
 		</main>
 	);
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,11 @@
 const Testimonials = () => {
-	return <div>Testimonials</div>;
+	return <section id="testimonials">
+		<div className="max-w-7xl mx-auto px-4 mt-20">
+			<div className="text-center mb-12">
+				
+			</div>
+		</div>
+	</section>;
 };
 
 export default Testimonials;
