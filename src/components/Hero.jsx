b4608fc8351b8d2e5151d129220b538ef340e1d0
Alan Webb <webbalan91939@gmail.com>
2025-07-15 11:10:05 +0100

    Update Hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,25 +10,25 @@ const Hero = () => {
 				<h1 className="text-5xl lg:text-8xl my-4 font-semibold tracking-tighter bg-gradient-to-t from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text text-transparent">
 					{HERO_CONTENT.mainHeading}
 				</h1>
-				<p className="py-8 mt-6 text-xl lg:text-2xl text-center text-white max-w-xl">
+				<p className="py-8 mt-6 text-xl lg:text-2xl bg-purple-900/50 rounded-xl text-center uppercase text-yellow-400 max-w-xl">
 					{HERO_CONTENT.subHeading}
 				</p>
 
-				<div className="py-6 space-x-4">
+				<div className="py-10 space-x-4">
 					<a
 						href="#"
-						className="inline-block bg-purple-600 hover:bg-purple-500 text-white border border-yellow-50 py-3 px-6 rounded-lg font-medium transition duration-200">
+						className="inline-block bg-purple-800 hover:bg-purple-500 text-white border border-yellow-50 py-3 px-6 rounded-lg font-medium transition duration-200">
 						{HERO_CONTENT.callToAction.primary}
 					</a>
 					<a
 						href="#"
-						className="inline-block bg-purple-600 hover:bg-purple-500 text-white border border-yellow-50 py-3 px-6 rounded-lg font-medium transition duration-200">
+						className="inline-block bg-purple-800 hover:bg-purple-500 text-white border border-yellow-50 py-3 px-6 rounded-lg font-medium transition duration-200">
 						{HERO_CONTENT.callToAction.secondary}
 					</a>
 				</div>
 
 				<div className="py-8">
-					<p className="text-white text-center text-xl md:text-2xl mb-8">
+					<p className="text-yellow-400 text-center text-xl md:text-2xl">
 						{HERO_CONTENT.formatsConverted}
 					</p>
 				</div>
