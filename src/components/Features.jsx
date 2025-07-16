07ac129c13f8595bcdfeef805eb202e98fd18938
Alan Webb <webbalan91939@gmail.com>
2025-07-16 15:12:50 +0100

    Update Features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,7 +4,7 @@ const Features = () => {
 	return (
 		<section id="features">
 			<div className="max-w-7xl mx-auto px-4 mt-20">
-				<div className="text-center mb-12">
+				<div className="text-center mb-8">
 					<h2 className="text-3xl lg:text-5xl mt-20 tracking-tighter bg-gradient-to-t from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text text-transparent">
 						{FEATURES_CONTENT.sectionTitle}
 					</h2>
@@ -15,12 +15,12 @@ const Features = () => {
 					{FEATURES_CONTENT.features.map((feature) => (
 						<div
 							key={feature.id}
-							className="flex flex-col items-center text-center w-full md:w-1/2 lg:w-1/3 p-6">
-							<div className="flex justify-center items-center mb-4">
+							className="flex flex-col items-center text-center w-full md:w-1/2 lg:w-1/3 p-16">
+							<div className="flex justify-center items-center text-yellow-500 mb-6">
 								{feature.icon}
 							</div>
-							<h3 className="text-xl">{feature.title}</h3>
-							<p className="mt-2 text-neutral-400">{feature.description}</p>
+							<h3 className="text-2xl bg-gradient-to-t from-yellow-100 via-yellow-300 to-yellow-500 bg-clip-text text-transparent">{feature.title}</h3>
+							<p className="mt-2 text-white text-xl">{feature.description}</p>
 						</div>
 					))}
 				</div>
